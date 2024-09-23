3ea45c0088861ade6c4649931e182f7dd6b060fb
bragisivarss <sigursteinnbragi@gmail.com>
2024-09-23 14:28:34 +0000

    first

---
+++ prufa/prufa/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using prufa;
+
+Console.WriteLine("Hello");
+
+List<int> userNumbers = [];
+
+string userInput;
+
+do
+{
+    WeatherData weather = new WeatherData();
+
+    weather.Temperature = 25;
+    weather.Humidity = 50;
+    weather.Scale = 'C';
+
+    weather.DisplayWeather();
+
+    weather.Convert();
+
+    weather.DisplayWeather();
+
+    Console.Write("Please enter a number (or press Enter to finish): ");
+    userInput = Console.ReadLine();
+
+    int parsedInput;
+
+    if (!String.IsNullOrEmpty(userInput) && Int32.TryParse(userInput, out parsedInput))
+    {
+        userNumbers.Add(parsedInput);
+    }
+    else if (!String.IsNullOrEmpty(userInput))
+    {
+        Console.WriteLine("Please enter a valid number");
+    }
+}
+while (!String.IsNullOrEmpty(userInput));
+
+if (userNumbers.Count > 0)
+{
+    int lowestNumber = userNumbers[0];
+    int highestNumber = userNumbers[0];
+
+    for (int i = 1; i < userNumbers.Count; i++)
+    {
+        if (userNumbers[i] > highestNumber)
+        {
+            highestNumber = userNumbers[i];
+        }
+        if (userNumbers[i] < lowestNumber)
+        {
+            lowestNumber = userNumbers[i];
+        }
+    }
+
+    Console.WriteLine($"Lowest number is: {lowestNumber}");
+    Console.WriteLine($"Highest number is: {highestNumber}");
+}
+else
+{
+    Console.WriteLine("No numbers were entered.");
+}
---
+++ prufa/prufa/WeatherData.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prufa
+{
+    internal class WeatherData
+    {
+        private double temperature;
+        private double humidity;
+        private char scale;
+
+        public double Temperature
+        {
+            get => temperature;
+            set
+            {
+                if (value < -60 || value > 60)
+                {
+                    Console.WriteLine("Error: Mistakes must have been made since the value seems unrealistic.");
+                }
+                else
+                {
+                    temperature = value;
+                }
+            }
+        }
+
+        public double Humidity
+        {
+            get => humidity;
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    Console.WriteLine("Error: Humidity must be between 0% and 100%.");
+                }
+                else
+                {
+                    humidity = value;
+                }
+            }
+        }
+
+        public char Scale
+        {
+            get => scale;
+            set
+            {
+                if (value == 'C' || value == 'F')
+                {
+                    scale = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Scale must be either 'C' or 'F'.");
+                }
+            }
+        }
+
+        public void Convert()
+        {
+            if (scale == 'C')
+            {
+                temperature = (temperature * 9 / 5) + 32;
+                scale = 'F';
+            }
+            else if (scale == 'F')
+            {
+                temperature = (temperature - 32) * 5 / 9;
+                scale = 'C';
+            }
+        }
+
+        public void DisplayWeather()
+        {
+            Console.WriteLine($"Current Temperature: {temperature}°{scale}");
+            Console.WriteLine($"Current Humidity: {humidity}%");
+        }
+    }
+}
